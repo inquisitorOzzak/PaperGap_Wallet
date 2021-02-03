9869fd50c770b902b22f0a6dbc496204736f62ec
robertcob <robertcob@icloud.com>
2021-02-03 17:53:12 +0000

    added homepage

--- venv/frontEnd/app.py
+++ venv/frontEnd/app.py
@@ -1,11 +1,11 @@
 from kivy.app import  App
 from kivy.uix.widget import Widget
+from view_Controllers.homepage import homepage
 
-class testpage(Widget):
-    pass
+
 
 class paperGap_Wallet(App):
     def build(self):
-        return testpage()
+        return homepage()
 
 paperGap_Wallet().run()
