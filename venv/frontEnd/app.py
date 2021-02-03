dcaffb592e2d81e59e6183c9a1c881d91b0c7168
robertcob <robertcob@icloud.com>
2021-02-03 19:53:59 +0000

    added skeletal structure for wallet creation view

--- venv/frontEnd/app.py
+++ venv/frontEnd/app.py
@@ -1,11 +1,13 @@
 from kivy.app import  App
 from kivy.uix.widget import Widget
 from view_Controllers.homepage import homepage
+from view_Controllers.create_A_Wallet import Create_Wallet
 
 
 
 class paperGap_Wallet(App):
     def build(self):
-        return homepage()
+        # return homepage()
+        return Create_Wallet()
 
 paperGap_Wallet().run()
