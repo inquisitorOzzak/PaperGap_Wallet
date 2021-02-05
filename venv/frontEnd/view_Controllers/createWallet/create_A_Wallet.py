005399963f24043e73519f3b83ac898570b121bf
robertcob <robertcob@icloud.com>
2021-02-05 18:31:18 +0000

    added UI for wallet creation page, and recovery phrase page

--- venv/frontEnd/view_Controllers/createWallet/create_A_Wallet.py
+++ venv/frontEnd/view_Controllers/createWallet/create_A_Wallet.py
@@ -5,30 +5,25 @@ from kivy.app import  App
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 
 # Designate our .kv design file
-Builder.load_file('wallet_build.kv')
-class Create_Wallet(Widget):
-        # info_Label = Label(
-        #     text = "In order to access your bitcoin wallet we will generate a random mnemonic",
-        #     font_size = 20,
-        #     width = self.width,
-        #     height = 60
-        #
-        # )
-        #
-        # word_Num_Dropdown = Spinner(
-        #     id = spinner_id,
-        #     text = "Word Number",
-        #     values = ["16", "20", "24"],
-        #
-        # )
+# Builder.load_file('wallet_build.kv')
+class Create_Wallet(FloatLayout):
+
     def word_Spinner_Clicked(self, value):
         #here we will be implementing some of the
         #backend bitcoin code
         print(value)
         print("/////////")
-        print(self.ids.click_label.text)
+
+    def coin_Spinner_Clicked(self, value):
+        print(value)
+        print("/////////")
+
+    def language_Spinner_Clicked(self, value):
+        print(value)
+        print("/////////")
 
 
 class wallet_Build(App):
